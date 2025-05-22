e1d435ed909cc817f4fcad00048d6e4a8262769c
Nguyen Tuan Anh <nguyentuananh.developer@gmail.com>
2025-05-22 22:22:18 +0700

    presentation

--- Sources/MistKit/UISheetPresentationController/_UISheetPresentationController.swift
+++ Sources/MistKit/UISheetPresentationController/_UISheetPresentationController.swift
@@ -1,5 +1,25 @@
 import UIKit
 
+public extension UISheetPresentationController {
+    var wantsFullScreen: Bool {
+        get {
+            return value(forKey: "wantsFullScreen") as! Bool
+        }
+        set {
+            setValue(newValue, forKey: "wantsFullScreen")
+        }
+    }
+    
+    var allowsInteractiveDismissWhenFullScreen: Bool {
+        get {
+            return value(forKey: "allowsInteractiveDismissWhenFullScreen") as! Bool
+        }
+        set {
+            setValue(newValue, forKey: "allowsInteractiveDismissWhenFullScreen")
+        }
+    }
+}
+
 public extension UISheetPresentationController.Detent {
     static func full() -> UISheetPresentationController.Detent {
         return value(forKey: "_fullDetent") as! UISheetPresentationController.Detent
